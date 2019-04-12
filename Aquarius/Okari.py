fb10c173f961b51b89dcca0c8c62c8089a728e18
nikminer <nikminerfeed@gmail.com>
2019-04-12 10:14:49 +0300

    Update Okari.py

--- Aquarius/Okari.py
+++ Aquarius/Okari.py
@@ -7,13 +7,14 @@ import os
 
 def AddMember(member):
     os.mkdir("Aquarius/src/Images/Users/"+member.name)
-    CreateFirstWelcomeMessage(member.avatar_url).save("Aquarius\\src\\Images\\Users\\"+member.name+"\\first"+".png", format="png")
-    return "Aquarius\\src\\Images\\Users\\"+member.name+"\\first"+".png"
+    CreateFirstWelcomeMessage(member.avatar_url).save("Aquarius/src/Images/Users/"+member.name+"/first"+".png", format="png")
+    return "Aquarius/src/Images/Users/"+member.name+"/first"+".png"
 
 
 def CreateFirstWelcomeMessage(memberAvatar):
-    Avatar = Image.open(BytesIO(requests.get(memberAvatar).content)).resize((125,125))
-    base = Image.open('Aquarius\\src\\Images\\NewLabmember.png')
+    Avatar = Image.open(BytesIO(requests.get(memberAvatar).content))
+    .thumbnail((125,125))
+    base = Image.open('Aquarius/src/Images/NewLabmember.png')
     dir(base)
     base.paste(Avatar,(12,11))
     return base

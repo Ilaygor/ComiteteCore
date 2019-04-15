fb99116fb5b18f799c20163f7140e94520fb8b93
nikminer <nikminerfeed@gmail.com>
2019-04-15 23:46:25 +0300

    Refactoring Memgen

--- Maid/Memes_name_subject_to_change.py
+++ Maid/Memes_name_subject_to_change.py
@@ -11,7 +11,7 @@ def CreateMem(memname,text):
     memdata=json.loads(open('Maid/src/Images/memes/{}.json'.format(memname)).read())
     base = Image.open('Maid/src/Images/memes/'+memdata['filename'])
     linecount=0
-    for line in textwrap.wrap(text,width=memdata['placewidth']/10):
+    for line in textwrap.wrap(text,width=memdata['placewidth']/(memdata['font-size']*0.5)):
         offset=linecount*memdata['font-size']
         if offset<memdata['placeheight']:
             AddText(line,(memdata['x'], memdata['y']+offset),base,size=memdata['font-size'],color=ImageColor.getrgb(memdata['textcolor']))

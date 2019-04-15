845d8bdcf8ffb653fe0b40ba433f53f6571b1585
nikminer <nikminerfeed@gmail.com>
2019-04-15 23:26:51 +0300

    memgen fixed

--- Maid/Memes_name_subject_to_change.py
+++ Maid/Memes_name_subject_to_change.py
@@ -11,9 +11,13 @@ def CreateMem(memname,text):
     memdata=json.loads(open('Maid/src/Images/memes/{}.json'.format(memname)).read())
     base = Image.open('Maid/src/Images/memes/'+memdata['filename'])
     linecount=0
-    for line in textwrap.wrap(text,width=memdata['placewidth']):
-        AddText(line,(memdata['x'], memdata['y']+linecount*memdata['font-size']),base,size=memdata['font-size'],color=ImageColor.getrgb(memdata['textcolor']))
-        linecount+=1
+    for line in textwrap.wrap(text,width=memdata['placewidth']/10):
+        offset=linecount*memdata['font-size']
+        if offset<memdata['placeheight']:
+            AddText(line,(memdata['x'], memdata['y']+offset),base,size=memdata['font-size'],color=ImageColor.getrgb(memdata['textcolor']))
+            linecount+=1
+        else:
+            break
     path="Maid/src/Images/Temp/"+memname+'_'+str(random.randint(0,100))+".png"
     base.save(path, format="png")
     return path

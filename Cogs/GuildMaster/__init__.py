114b6ecb5169bfcf9fafc61d530e8b0ee0d82370
nikminer <nikminerfeed@gmail.com>
2019-09-04 16:20:21 +0300

    v 0.5.6

--- Cogs/GuildMaster/__init__.py
+++ Cogs/GuildMaster/__init__.py
@@ -15,7 +15,36 @@ class GuildMaster(commands.Cog):
                 await member.add_roles(member.guild.get_role(int(jr)))
 
             for i in SQLWorker.GetRoles(member.guild.id,member.id):
-                await member.add_roles(member.guild.get_role(int(i[0])))
+                try:
+                    await member.add_roles(member.guild.get_role(int(i[0])))
+                except AttributeError:
+                    SQLWorker.DelRole(i[0])
+                except discord.errors.Forbidden:
+                    pass
+    
+    @commands.command(name="returnroles", help="Возвращают текущему пользователю роли если они вдруг автоматически не вернулись при возвращении на сервер",usage="[@Пользователь]",brief="Возвращение ролей")
+    async def returnroles(self,ctx,member=None):
+        if (member):
+            author=await commands.MemberConverter().convert(ctx,member)
+        else:
+            author=ctx.author
+
+        embed=discord.Embed(title="Cписок ролей:", description="Роли пользователя "+author.name,color=author.color)
+        for i in SQLWorker.GetRoles(ctx.guild.id,author.id):
+            role= ctx.guild.get_role(int(i[0]))
+            if role:
+                if role in author.roles:
+                    embed.add_field(name=role.name, inline=True, value=str(i[0]))
+                else:
+                    try:
+                        await author.add_roles(role)
+                        embed.add_field(name=role.name+" (Возврашено)", inline=True, value=str(i[0]))
+                    except discord.errors.Forbidden:
+                        embed.add_field(name=role.name+" (Missing Permissions)", inline=True, value=str(i[0]))
+            else:
+                embed.add_field(name="Роль не найдена и удалена из БД", inline=True, value=str(i[0]))
+                SQLWorker.DelRole(i[0])
+        await ctx.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_update(self,before,after):
